db65ac4e1b396ecba6554593b72a1f0a7b475e2a
Nick Strayer <nstrayer@uvm.edu>
2014-09-13 16:09:20 -0400

    javascript is all up and running with max percentages.

--- HW01/work/polioVis/script.js
+++ HW01/work/polioVis/script.js
@@ -1,17 +1,30 @@
-d3.json("listData.json", function(data){
+d3.json("polioPercent.json", function(data){
 
-var states = Object.keys(data[1]) 
-percents = []
-for (var i = 1; i < data.length; i++){ 					//loop through list of dictionaries
-	for (var j = 0; j < states.length; j++) { 		    //inside the dictionaries loop through the states
-		var state = states[j]
-		percents.push(data[i][state])
+var years = Object.keys(data) 
+
+//console.log(data[years[2]])
+var maxPercentage = 0
+
+for (var i = 0; i < years.length; i++){
+	var maxPerc = d3.max(data[years[i]], function(d){return d["polioPercent"]})
+	if (maxPerc > maxPercentage) {
+		maxPercentage = maxPerc
 	}
 }
 
-maxPercent = d3.max(percents, function(d){return d})
-minPercent = d3.min(percents, function(d){ if (d !== 0) {return d} } )
-console.log("Max: " + maxPercent + " Min: " + minPercent)
+console.log(maxPercentage)
+
+// percents = []
+// for (var i = 1; i < data.length; i++){ 					//loop through list of dictionaries
+// 	for (var j = 0; j < states.length; j++) { 		    //inside the dictionaries loop through the states
+// 		var state = states[j]
+// 		percents.push(data[i][state])
+// 	}
+// }
+
+// maxPercent = d3.max(percents, function(d){return d})
+// minPercent = d3.min(percents, function(d){ if (d !== 0) {return d} } )
+// console.log("Max: " + maxPercent + " Min: " + minPercent)
 
 //percents.forEach(function(element){console.log(element) }) //example of forEach syntax, valubale to have. 
 })
