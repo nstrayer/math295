ebab6f6d870280a9405e0d6c06d93a46802d23c9
Nick Strayer <nstrayer@uvm.edu>
2014-09-14 15:11:52 -0400

    have a working d3 visualization up for part 2c

--- HW01/work/polioVis/script.js
+++ HW01/work/polioVis/script.js
@@ -2,29 +2,143 @@ d3.json("polioPercent.json", function(data){
 
 var years = Object.keys(data) 
 
-//console.log(data[years[2]])
-var maxPercentage = 0
+//Set up the size of the graph
+var height  = 500,
+	width   = 1000,
+	padding = 10;
+
+var maxPercentage = 0, //need to seed this to find max and min percents. 
+	minPercentage = 0.2;
 
 for (var i = 0; i < years.length; i++){
-	var maxPerc = d3.max(data[years[i]], function(d){return d["polioPercent"]})
+	var maxPerc    = d3.max(data[years[i]], function(d){return d["polioPercent"]})
+	var minPerc    = d3.min(data[years[i]], function(d){ if (d["polioPercent"] !== 0) {return d["polioPercent"]} } )
 	if (maxPerc > maxPercentage) {
 		maxPercentage = maxPerc
 	}
+	if (minPerc < minPercentage) {
+		minPercentage = minPerc
+	}
 }
 
-console.log(maxPercentage)
+console.log("Max = " + maxPercentage + " Min = " + minPercentage)
 
-// percents = []
-// for (var i = 1; i < data.length; i++){ 					//loop through list of dictionaries
-// 	for (var j = 0; j < states.length; j++) { 		    //inside the dictionaries loop through the states
-// 		var state = states[j]
-// 		percents.push(data[i][state])
-// 	}
-// }
+var yScale = d3.scale.linear()
+				.domain([0, maxPercentage])
+				.range([0, height - padding]) //-50 for fitting text
 
-// maxPercent = d3.max(percents, function(d){return d})
-// minPercent = d3.min(percents, function(d){ if (d !== 0) {return d} } )
-// console.log("Max: " + maxPercent + " Min: " + minPercent)
+var xScale = d3.scale.ordinal()
+				.domain(d3.range(data[31].length + 1))
+				.rangeRoundBands([0,width],0.2);
 
-//percents.forEach(function(element){console.log(element) }) //example of forEach syntax, valubale to have. 
+var svg = d3.select("#visualization")
+			.append("svg")
+			.attr("height", height)
+			.attr("width", width)
+
+function fillColor(value){
+	if (value != 0) {
+		return "steelblue"
+	} else {
+		return "black"} }
+
+svg.selectAll("rect")
+	.data(data[31], function(d) {return d["state"]} )
+	.enter()
+	.append("rect")
+	.attr("x", function(d,i) {return xScale(i) })
+	.attr("y", function(d){return height - yScale(d["polioPercent"])})
+	.attr("height", function(d){return yScale(d["polioPercent"])})
+	.attr("width", xScale.rangeBand())
+	.attr("fill", function(d) { return fillColor(d["polioPercent"])})
+	.on("click", function(){
+		history(31)
+	})
+ 
+svg.selectAll("text")
+	.data(data[31], function(d) {return d["state"]} )
+	.enter()
+	.append("text")
+	.attr("class", "stateNames")
+	.attr("x", function(d,i) {return xScale(i) + (xScale.rangeBand())/2 })
+	.attr("y", function(d){return (height - 3) - yScale(d["polioPercent"])})
+	.text(function(d){return d["state"]})
+	.attr("text-anchor", "middle")
+	.attr("font-family", "optima")
+	.attr("font-size", 9)
+
+
+svg.selectAll("text")
+	.append("text")
+	.attr("x", width - 20)
+	.attr("y", height - 300)
+	.text("1931")
+	.attr("font-family", "optima")
+	.attr("font-size", 30)
+
+
+function changeYear(year){
+	d3.selectAll("rect")
+		.data(data[year], function(d) {return d["state"]} )
+		.transition()
+		.attr("y", function(d){return height - yScale(d["polioPercent"])})
+		.attr("height", function(d){return yScale(d["polioPercent"])})
+		.attr("fill", function(d) { return fillColor(d["polioPercent"])})
+
+	d3.selectAll(".stateNames")
+		.data(data[year], function(d) {return d["state"]})
+		.transition()
+		.attr("y", function(d){return (height - 3) - yScale(d["polioPercent"])})
+
+	d3.select("#currentYear")
+		.text("Currently showing: 19" + year)
+}
+
+//slider code
+$(function() {
+            $( "#slider" ).slider({
+               value: 31,
+               ticks: true,
+               min: 31,
+               max: 69,
+               step: 1,
+               animate:"slow",
+               orientation: "horizontal",
+               slide: function( event, ui ) { changeYear(ui.value) }
+           });
+         });
+
+var theYear = 31
+
+function history(theYear){
+	d3.selectAll("rect")
+		.data(data[theYear], function(d) {return d["state"]} )
+		.transition()
+		.ease("log")
+		.duration(500)
+		.attr("y", function(d){return height - yScale(d["polioPercent"])})
+		.attr("height", function(d){return yScale(d["polioPercent"])})
+		.attr("fill", function(d) { return fillColor(d["polioPercent"])})
+		.each("end", function(d,i){ 
+			if (theYear < 69) {
+				history(theYear + 1)
+				console.log("changing it")
+			}} )
+
+	d3.selectAll(".stateNames")
+		.data(data[theYear], function(d) {return d["state"]})
+		.transition()
+		.ease("log")
+		.duration(500)
+		.attr("y", function(d){return (height - 3) - yScale(d["polioPercent"])})
+
+	d3.select("#currentYear")
+		.text("Currently showing: 19" + theYear)
+
+	$("#slider").slider('value', theYear);
+}
+
+d3.select("#startButton")
+	.on("click", function(){history(31)})
+
 })
